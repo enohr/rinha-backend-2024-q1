fedf050a8dff0c99091b22b826626127399410d2
Eduardo Nohr Lessa <eduardonohr@gmail.com>
2024-03-10 15:11:59 -0300

    Improvements on postgresql performance

--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE UNLOGGED TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     limite INTEGER NOT NULL,
     saldo INTEGER NOT NULL DEFAULT 0,
     CONSTRAINT chk_limite CHECK (-saldo <= limite)
 );
 
-CREATE TABLE IF NOT EXISTS transacoes (
+CREATE UNLOGGED TABLE IF NOT EXISTS transacoes (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     valor INTEGER NOT NULL,
@@ -15,10 +15,13 @@ CREATE TABLE IF NOT EXISTS transacoes (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE INDEX idx_transacoes_realizada_em ON transacoes(realizada_em DESC, user_id);
+CREATE INDEX idx_transacoes_user_id ON transacoes(user_id);
+
 INSERT INTO users(id, limite)
 VALUES 
     (1, 100000),
     (2, 80000),
     (3, 1000000),
     (4, 10000000),
-    (5, 500000);
+    (5, 500000)
